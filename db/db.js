28840d37c882c622e2233a6460e7fa359477ca9b
sahanft <udakarafernando2001@gmail.com>
2024-06-11 13:17:11 +0530

    update

--- db/db.js
+++
@@ -1,4 +0,0 @@
-export var customer_db = [];
-export var item_db = [];
-export var order_db = [];
-export var order_details_db = [];
--- model/ItemModel.js
+++
@@ -1,8 +0,0 @@
-export class ItemModel{
-    constructor(itemCode,itemName,price,qty) {
-        this.itemCode = itemCode;
-        this.itemName = itemName;
-        this.price = price;
-        this.qty = qty;
-    }
-}
